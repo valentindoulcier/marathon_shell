f366e8f704c65fbed7db919fa6f5bf19528d22c2
Valentin DOULCIER <valentin.doulcier@gmail.com>
2012-03-13 11:24:51 +0100

    [ PIL - Marathon Shell ] - Installation
    - Configuration du projet Eclipse
    - Ajout de fond d'écran dans les drawables

--- gen/com/marathon_shell/R.java
+++ gen/com/marathon_shell/R.java
@@ -10,14 +10,21 @@ package com.marathon_shell;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int Noir=0x7f040000;
+    }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int auroralized_aqua=0x7f020000;
+        public static final int auroralized_aqua_dock=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int ic_sortie=0x7f020003;
+        public static final int logo_eco_marathon_shell=0x7f020004;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int application=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f050001;
+        public static final int hello=0x7f050000;
     }
 }
---
+++ src/com/marathon_shell/MarathonShellActivity.java
@@ -0,0 +1,14 @@
+package com.marathon_shell;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MarathonShellActivity extends Activity {
+	
+	
+    /** Called when the activity is first created. */
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.application);
+    }
+}
