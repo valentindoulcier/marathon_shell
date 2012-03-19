5433b45eccad57b4638173baf9f89f16204df3e5
Valentin DOULCIER <valentin.doulcier@gmail.com>
2012-03-19 09:48:12 +0100

    [ PIL - Marathon Shell ] - Développement
    - Ajout de Point
      < Représente un point de la course >
    - Ajout de Course
      < Contient toutes les informations d'une course >

---
+++ src/data/marathon_shell/Point.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package data.marathon_shell;
+
+/**
+ * Classe contenant les fonctions principales utilisées par l'application.
+ * 
+ * @author Valentin DOULCIER
+ * @version 1.0
+ * @see Application
+ *
+ */
+public class Point {
+
+	
+	/****************************
+	 * VARIABLES
+	 ****************************/
+	
+	/**
+	 * Ces deux variables sont utilisées pour un affichage clair dans les Logs.
+	 */
+	@SuppressWarnings("unused")
+	private String LogTag = "Marathon Shell";
+	@SuppressWarnings("unused")
+	private String Class = "POINT - ";
+	
+	
+	
+	/****************************
+	 * CONSTRUCTEURS
+	 ****************************/
+	
+	
+	/****************************
+	 * ACCESSEURS
+	 ****************************/
+	
+	
+	/****************************
+	 * METHODES
+	 ****************************/
+	
+}
